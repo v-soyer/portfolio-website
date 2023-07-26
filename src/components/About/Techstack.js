68e8127f786a9c41036cc84bc7651b59340b335a
soyerv <valentin.soyer46@gmail.com>
2023-07-26 18:39:52 +0200

    feat(Toolstack): add text appearing inside the boxes

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -14,7 +14,8 @@ import {
   SiHaskell,
   SiDocker,
   SiMongodb,
-  SiTypescript
+  SiTypescript,
+  SiNeo4J
 } from "react-icons/si";
 
 
@@ -23,39 +24,55 @@ function Techstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
+        <div class="text">Javascript</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
+        <div class="text">Node.JS</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiNestjs />
+        <div class="text">NestJS</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiPython />
+        <div class="text">Python</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiDjango />
+        <div class="text">Django</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiDocker />
+        <div class="text">Docker</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiMongodb />
+        <div class="text">MongoDB</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
+        <div class="text">Postgres</div>
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiNeo4J />
+        <div class="text">Neo4J</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
+        <div class="text">Git</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiTypescript/>
+        <div class="text">Typescript</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiHaskell />
+        <div class="text">Haskell</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiC />
+        <div class="text">C</div>
       </Col>
     </Row>
   );
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -15,24 +15,31 @@ function Toolstack() {
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
       <Col xs={4} md={2} className="tech-icons">
         <SiLinux />
+        <div class="text">Linux</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGithub />
+        <div class="text">Github</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiGitlab />
+        <div class="text">Gitlab</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
+        <div class="text">VS Code</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
+        <div class="text">Postman</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiJenkins />
+        <div class="text">Jenkins</div>
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <TbBrandOffice />
+        <div class="text">Office 365</div>
       </Col>
     </Row>
   );
