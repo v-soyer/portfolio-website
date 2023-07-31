91f7ea22d86776f4fb609eb5b24fefd2d61eb15f
soyerv <valentin.soyer46@gmail.com>
2023-07-31 20:46:24 +0200

    update(Projects): update the project section

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
 
 function ProjectCards(props) {
@@ -9,29 +8,34 @@ function ProjectCards(props) {
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        {props.isLinked && (
+          <Card.Title><b><a href="http://www.google.com" rel="noopener noreferrer" target="_blank"><b>{props.title}</b></a></b></Card.Title>
+        )}
+        {!props.isLinked && (
+          <Card.Title><b>{props.title}</b></Card.Title>
+        )}
+        <br />
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+        {/* If the component don't have ghLink it will render the below component  */}
 
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
+        {props.ghLink && (
+          <div>
+            <br />
+            <Button variant="primary" href={props.ghLink} target="_blank">
+              <BsGithub /> GitHub
+            </Button>
+          </div>
+        )}
+        
+        {/* If not, it will render the below component  */}
+
+        {!props.ghLink && props.enterpriseLogo && (
+          <div>
+            <Card.Img variant="top" href="https://www.accenture.com/fr-fr" src={props.enterpriseLogo} alt="card-img" style={{ width: "70%", height: "auto" }}/>
+          </div>
         )}
       </Card.Body>
     </Card>
